6beba8e1839dcba73e4286f44be83a73b69aec26
Ivan Shapovalov <sluvkann@gmail.com>
2021-06-21 13:00:19 +0300

    Add static files hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppService } from './app.service';
 import environment from '../other/environment';
 import { Game, GameSchema } from '../schemas/game.schema';
 import { AppGateway } from './app.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot(environment.databaseConnectionString, { useFindAndModify: false }),
     MongooseModule.forFeature([{ name: Game.name, schema: GameSchema }]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../shared'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
